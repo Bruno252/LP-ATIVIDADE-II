273abbc87f8e334e036caadbf3898586144cbcbe
Bruno252 <143636176+Bruno252@users.noreply.github.com>
2023-11-08 10:18:27 -0300

    Atualizar o Questao 14.c

--- Questao 14.c
+++ Questao 14.c
@@ -1,33 +1,39 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	
-	int numero=0,i=0,par=0,somaPar=0,impar=0,somaImpar=0;
-	float mediaPar,mediaImpar;
-	
-	do{
-	
-	printf("%d Numero",i);
-	scanf("%d",&numero);
-			
-	if(numero%2==0){
-		par++;
-		somaPar+=numero;
-		
-	}else{
-		impar++;
-		somaImpar+=numero;
-		
-	}
+int main() {
 
-mediaPar=somaPar/par;
-mediaImpar=somaImpar/impar;
-	
-	}while(numero>0);
-	
-	printf("\nMedia de Par:%.2f\n",mediaPar);
-	printf("Media de Impar:%.2f",mediaImpar);
-	
-	return 0;
+    int numero=0,i=0,par=0,somaPar=0,impar=0,somaImpar=0;
+    float mediaPar,mediaImpar;
+
+    do {
+        i++;
+        printf("%d Numero:",i);
+        scanf("%d",&numero);
+
+        if(numero<=0) {
+            break;
+
+        }
+
+        if(numero%2==0) {
+            par++;
+            somaPar+=numero;
+
+        } else {
+            impar++;
+            somaImpar+=numero;
+
+        }
+
+        mediaPar=somaPar/par;
+        mediaImpar=somaImpar/impar;
+
+    } while(numero>0);
+
+    printf("\nMedia de Par:%.2f\n",mediaPar);
+    printf("Media de Impar:%.2f",mediaImpar);
+
+    return 0;
 }
