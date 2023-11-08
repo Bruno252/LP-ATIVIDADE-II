0c9fa17db05cc5c889b1938a2f92d117a3981078
Bruno252 <143636176+Bruno252@users.noreply.github.com>
2023-11-08 18:48:56 -0300

    Atualizar o Questao 15.c

--- Questao 15.c
+++ Questao 15.c
@@ -1,41 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	
-	int escolha;
-	float valor,fahre,celsius;
-	
-	printf("Digite:\n");
-	printf("1 | para Celsius Para Fahrenheit\n");
-	printf("2 | para Fahrenheit para Celsius\n");
-	printf("\nEscolha:");
-	scanf("%d",&escolha);
-	
-	printf("\nValor");
-	scanf("%f",&valor);
-	
-	switch(escolha){
-		
-		case 1:
-			
-		fahre=(valor * 9/5)+32;
-		printf("Fahrenheit:%.2f",fahre);
-		break;
-		
-		case 2:	
-		
-		celsius=(valor * -32)*5/9;
-		printf("Celsius:%.2f",celsius);
-		break;
-		
-		default:
-			
-			printf("Escolha Invalida");
-		break;
-		
-	
-	}
-	
-	return 0;
+int main() {
+
+    int escolha;
+    float valor,fahre,celsius,kelvin;
+
+    do {
+
+        printf("\nDigite:\n");
+        printf("1 | Conversao de Celsius\n");
+        printf("2 | Conversao de Farhrenheit\n");
+        printf("3 | conversao de Kelvin");
+        printf("\nEscolha:");
+        scanf("%d",&escolha);
+        
+        if(escolha==0){
+        
+        break;
+        
+        }
+
+        printf("\nValor");
+        scanf("%f",&valor);
+
+        switch(escolha) {
+
+        case 1:
+
+            fahre=(valor * 9/5)+32;
+            printf("Fahrenheit:%.2f\n",fahre);
+
+            kelvin=(valor + 273.15);
+            printf("Kelvin:%.2f \n",kelvin);
+
+            break;
+
+
+        case 2:
+
+            celsius=(valor * -32)*5/9;
+            printf("Celsius:%.2f\n",celsius);
+
+
+            kelvin=(valor - 32) * 5/9 + 273.15;
+            printf("Kelvin:%.2f \n",kelvin);
+
+
+            break;
+
+        case 3:
+
+            celsius=(valor - 273.15);
+            printf("Celsius:%.2f\n",celsius);
+
+            fahre=(valor - 273.15) * 9/5 + 32;
+            printf("Fahrenheit:%.2f\n",fahre);
+
+            break;
+            
+        default:
+
+            printf("Escolha Invalida");
+            break;
+
+
+        }
+    } while(escolha!=0);
+    return 0;
 }
